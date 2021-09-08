89eac92290d1a44a87689c7373e5de5029945ca4
unknown <shelite17@gmail.com>
2021-09-07 23:15:11 -0600

    Updated login page
    Updated formatting

--- admin/login.php
+++ admin/login.php
@@ -6,16 +6,19 @@
         if ($username == "galleryadmin" && $password == "securepw") {
             $_SESSION['05e252a0-8c03-4b70-95fc-5f64a6a2df6c'] = session_id();
             header("Location:../index.php");
+            include("../includes/header.php");
         }
         else {
+            include("../includes/header.php");
             echo "<br><p class=\"text-danger\">Incorrect login credentials</p>";
         }
     }
+    else {
+        include("../includes/header.php");
+    }
 
-    include("../includes/header.php");
-    
 
-    echo "<h2 class=\"text-dark\">Login</h2>";
+echo "<h2 class=\"text-dark\">Login</h2>";
 echo "<form action=\"";
 echo htmlspecialchars($_SERVER['PHP_SELF']);
 echo "\" method=\"post\" name=\"login\">
